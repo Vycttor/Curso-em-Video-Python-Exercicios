715f8e4c274b319b416089fd9d0f976aba4540df
Victor <87349107+Vycttor@users.noreply.github.com>
2024-11-19 15:47:24 -0300

    Update Ex060.py

--- Ex060.py
+++ Ex060.py
@@ -4,3 +4,19 @@ Ex. 60. Faça um programa que leia um número qualquer e mostre o seu fatorial
 Fatorial de 5.
 Ex: 5! = 5x4x3x2x1 = 120
 '''
+
+
+fat = 1
+cont =1
+
+numero = int(input('Informe o numero: '))
+
+while cont <= numero:
+
+    fat *= cont
+    #print(fat)
+    cont += 1
+    
+print(f'{numero}! = {fat}')    
+print('fim')
+  
