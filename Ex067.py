b6221fd5313a2df17b84c5f6964cde8fd8c00ec0
Victor <87349107+Vycttor@users.noreply.github.com>
2024-11-29 21:29:23 -0300

    Aprendendo While

---
+++ Ex067.py
@@ -0,0 +1,6 @@
+'''
+Ex 67. Faça um programa que mostre a tabuada de vários numeros, um de cada vez, para cada valor digitado pelo usuário. O programa será interropido quando o número solicitado for negativo.
+'''
+
+while True:
+    
---
+++ Ex070.py
@@ -0,0 +1,8 @@
+'''
+Ex. 70. Crie um progrma que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar. No final, mostre:
+
+a) Qual o total gasto na compra.
+b) Quantos produtos custam mais de R$ 1000.
+c) Qual é o nome do produto mais barato.
+
+'''
