06b8530831abeb9087f0134eb035a291cdca76c1
Victor <87349107+Vycttor@users.noreply.github.com>
2024-11-29 23:10:04 -0300

    while

--- Ex067.py
+++ Ex067.py
@@ -2,5 +2,18 @@
 Ex 67. Faça um programa que mostre a tabuada de vários numeros, um de cada vez, para cada valor digitado pelo usuário. O programa será interropido quando o número solicitado for negativo.
 '''
 
+
+
 while True:
+    print('=='*25)
+    t = int(input('Informe a tabuada que deseja: '))
     
+    if t == -2:
+        break
+    for c in range(0,11):      
+        #print(f'|{num} X {c}  = {resultato}|')
+        print("{} X  {} = {} ".format(t,c, t*c))       
+    print('=='*25)
+    print('**Iforme o próximo valor ou -2 para ENCERRAR')
+    
+print('Programa encerrado')
--- Ex069.py
+++ Ex069.py
@@ -5,3 +5,24 @@ a) Quanta pessoas tem mais de 18 anos.
 b) Quantos homens foram cadastrados.
 c) Quantas mulheres tem menos de 20 anos.
 '''
+
+cont_h = cont_m = cont_idade = 0
+
+while True:
+    sexo = str(input('Informe o sexo: ')).strip().lower()
+    idade = int(input('Informe a idade: '))
+    if sexo == 999 and idade ==999:
+        break
+    if idade >= 18:
+        cont_idade = cont_idade + 1
+    if sexo  == 'm':
+        cont_h = cont_h + 1
+    if idade <= 20 and sexo == 'f':
+        cont_idade = cont_idade +1
+        cont_m = cont_h + 1
+
+
+print(f'A {cont_idade} maior de 18 anos')
+
+
+    
