0f776b687f25745bc189db1c2be36ff735b8efb2
Victor <87349107+Vycttor@users.noreply.github.com>
2024-12-01 00:50:34 -0300

    aprendendo whil..

--- Ex069.py
+++ Ex069.py
@@ -8,21 +8,30 @@ c) Quantas mulheres tem menos de 20 anos.
 
 cont_h = cont_m = cont_idade = 0
 
-while True:
-    sexo = str(input('Informe o sexo: ')).strip().lower()
+while True: 
     idade = int(input('Informe a idade: '))
-    if sexo == 999 and idade ==999:
-        break
+    sexo = ' ' 
+    while sexo not in 'mf': # laço não permite avaçar enquanto não for digitado o valor correto do tipo.
+        sexo = str(input('Informde o sexo:[M/F]')).strip().lower()
+
     if idade >= 18:
         cont_idade = cont_idade + 1
+
     if sexo  == 'm':
         cont_h = cont_h + 1
-    if idade <= 20 and sexo == 'f':
-        cont_idade = cont_idade +1
-        cont_m = cont_h + 1
 
+    if idade < 20 and sexo == 'f':
+        cont_m = cont_m + 1
 
-print(f'A {cont_idade} maior de 18 anos')
+    continuar = ' '
+    while continuar not in 'sn': #Condição de parada usando o tipo. 
+        continuar = str(input('Continuar:[S/N] '))
+    if continuar == 'n': # Casso for digitado n, se torna verdadeiro e sai do laço.
+        break
+
+print(f'Total de pessas com mais de 18 anos: {cont_idade}')
+print(f'Ao todo temos {cont_h} homens cadastrados')
+print(f'E temos {cont_m} mulheres com menos de 20 anos')
 
 
     
--- Ex070.py
+++ Ex070.py
@@ -6,3 +6,27 @@ b) Quantos produtos custam mais de R$ 1000.
 c) Qual é o nome do produto mais barato.
 
 '''
+soma_compra = produto_acima1000 = 0
+
+while True:
+    nome_produto = str(input('Informe o nome do produto: ')).strip().upper()
+    preco_produto = float(input('Informe o valor do produto:R$ '))
+    
+    if preco_produto > 0: 
+        soma_compra = soma_compra + preco_produto
+    if preco_produto >= 1000:
+        produto_acima1000 = produto_acima1000 + 1
+    #if preco_produto > 0:
+     #   menor = nome_produto
+    #else:
+
+        
+
+    continuar = ' '
+    while continuar not in 'sn':
+        continuar = str(input('Continuar:[S/N] ')).strip()
+    if continuar == 'n':
+        break
+    #print('sai do laço....')
+print(f'preço do produto {soma_compra}')
+print(f'Produto acima de 1000 {produto_acima1000}')
