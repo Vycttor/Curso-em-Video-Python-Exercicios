291563bbb2d2346b7e174769d741c940edd8daa7
Victor <87349107+Vycttor@users.noreply.github.com>
2024-12-01 18:11:20 -0300

    Apren... while

--- Ex070.py
+++ Ex070.py
@@ -6,27 +6,46 @@ b) Quantos produtos custam mais de R$ 1000.
 c) Qual é o nome do produto mais barato.
 
 '''
-soma_compra = produto_acima1000 = 0
+soma_compra = produto_acima1000 = maior = menor = contador =0
+barato = caro = ' '
 
 while True:
     nome_produto = str(input('Informe o nome do produto: ')).strip().upper()
     preco_produto = float(input('Informe o valor do produto:R$ '))
+
+    #Incrementadores
+    contador = contador + 1 # Contado para condição de comparação de itens mais caros e mais baratos.
+    soma_compra = soma_compra + preco_produto
     
-    if preco_produto > 0: 
-        soma_compra = soma_compra + preco_produto
+    #if preco_produto > 0: #Esse if não é cenessário, porém funciona da mesma forma.
+        #soma_compra = soma_compra + preco_produto
+
     if preco_produto >= 1000:
         produto_acima1000 = produto_acima1000 + 1
-    #if preco_produto > 0:
-     #   menor = nome_produto
-    #else:
 
         
-
+    if contador == 1:
+          maior = preco_produto
+          caro = nome_produto
+          menor = preco_produto
+          barato = nome_produto
+    else:
+         if preco_produto > maior:
+            maior = preco_produto
+            caro = nome_produto
+         if preco_produto < menor:
+            menor = preco_produto
+            barato = nome_produto
+            
     continuar = ' '
     while continuar not in 'sn':
-        continuar = str(input('Continuar:[S/N] ')).strip()
+        continuar = str(input('Continuar:[S/N] ')).strip().lower()[0]
     if continuar == 'n':
         break
-    #print('sai do laço....')
-print(f'preço do produto {soma_compra}')
-print(f'Produto acima de 1000 {produto_acima1000}')
+print('sai do laço....')
+print('\n')
+print(f'Valor total da compra R$ {soma_compra:.2f} reais')
+print(f'Produto acima de R$1000,00: {produto_acima1000} produto(s).')
+print(f'O nome do produto mais barato: {barato} que custa R$ {menor:.2f} reais')
+print(f'O nome do produto mais caro: {caro} que custa R$ {maior:.2f} reais')
+
--- Ex071.py
+++ Ex071.py
@@ -3,3 +3,31 @@ Ex 71. Crie um programa que simule o funcionamento de um caixa eletrônico. No i
 
 OBS: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
 '''
+
+print('='*30)
+print('{:^30}'.format('BANCO CEV'))
+print('='*30)
+
+valor = int(input('Qual valor você quer sacar? R$'))
+total = valor
+cedula = 50
+totcedula = 0
+
+while True:
+    if total >= cedula:
+        total -= cedula
+        totcedula += 1
+    else:
+        if totcedula > 0:
+            print(f'Total de {totcedula} cédulas de R$ {cedula}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totcedula = 0
+        if total == 0:
+            break
+print('='*30)
+print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
