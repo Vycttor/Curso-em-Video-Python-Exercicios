55e03e8eec0e1aa1775af9f1f4ffb5776355827a
Victor <87349107+Vycttor@users.noreply.github.com>
2024-12-16 23:38:50 -0300

    Update Ex072.py

--- Ex072.py
+++ Ex072.py
@@ -4,3 +4,7 @@ Ex 72 Crie um programa que tenha uma tupla totalmente preechida com uma contagem
 Seu programa deverá ler um número pelo teclado (etre 0 e 20) e mostrá-lo por extenso.
 
 '''
+
+tupla = ('Um', 'Dois')
+
+numero = int(input('Digite um numero'))
