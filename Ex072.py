47fcc500e901873e01397e021a116ebbb8f1d73d
Victor <87349107+Vycttor@users.noreply.github.com>
2025-01-09 19:58:44 -0300

    atualizado

--- Ex072.py
+++ Ex072.py
@@ -5,6 +5,14 @@ Seu programa deverá ler um número pelo teclado (etre 0 e 20) e mostrá-lo por 
 
 '''
 
-tupla = ('Um', 'Dois')
+cont = ('zero', 'Um', 'Dois', 'três', 'quatro', 'cinco',
+         'seis', 'sete', 'oito', 'nove', 'dez',
+         'onze', 'doze', 'treze', 'catoze', 'quinze', 
+         'dezesseis', 'dezessete', 'dezoito','dezenove', 'vinte')
 
-numero = int(input('Digite um numero'))
+while True:
+    numero = int(input('Digite um numero Nº: '))
+    if 0 <= numero <= 20:
+        break
+    print('Tente novamente. ', end='')
+print(f'Você digitou o número {cont[numero]}')
--- Ex073.py
+++ Ex073.py
@@ -6,3 +6,24 @@ B) Os últimos 4 colocados na tabela.
 C) Uma lista com os times em ordem alfabética.
 D) Em que posição na tabela está o time da Chapecoense.
 '''
+
+times_brasileirao_2019 = (
+    'Flamengo', 'Palmeiras', 'Santos', 'Grêmio', 'Athletico-PR',
+    'São Paulo', 'Internacional', 'Corinthians', 'Fortaleza', 'Goiás',
+    'Bahia', 'Vasco', 'Atlético-MG', 'Fluminense', 'Botafogo',
+    'Ceará', 'Cruzeiro', 'CSA', 'Chapecoense', 'Avaí'
+)
+#método sorted permite ordenar a tupla em ordem alfaética.
+
+ordem_alfabetica = sorted(times_brasileirao_2019)
+
+print('\n')
+print('===='*15)
+print(f'Os 5 primeiros colocados são: {times_brasileirao_2019[0:5]}')
+print('===='*15)
+print(f'Os 4 ultimos colocados são: {times_brasileirao_2019[16:20]}')
+print('===='*15)
+print(f'Times em ordem alfabética: {ordem_alfabetica}')
+print('===='*15)
+print(f'A {times_brasileirao_2019[18]} encontra-se na 19º posição')
+print('===='*15)
