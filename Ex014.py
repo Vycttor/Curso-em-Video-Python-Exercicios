494bfdd31d53eca8a49bb9826dcf581a3927f2f5
Victor <87349107+Vycttor@users.noreply.github.com>
2025-05-06 01:48:03 -0300

    Refatorado

--- Ex014.py
+++ Ex014.py
@@ -4,16 +4,11 @@
 #   F = 9 x C + 32
 #       ------
 #         5
-
-# Função converte graus celsius em Fiefahrenheit
-def convertf(c):
-    b = (c*9)/5
-    fiefah = b + 32
-    print("{:.0f}º graus ceslsius convertiro para Fiefahrenheit é {:.0f}º Fiefahrenheits".format(c,fiefah))
-
-print("PROGRAMA CONVERTE GRAUS CELSIUS EM FIEFAHRENHEIT\n")
+print("""PROGRAMA CONVERTE GRAUS CELSIUS EM FIEFAHRENHEIT
+      """)
 c = int(input("Digite a temperatura em graus celsius: "))
 print("\n")
+#Formula to calculate Cº to Fª
+fiefah = 32 + (c*9) / 5
 
-#Chama/invoca a funçao para conversão
-convertf(c)
+print("{:.0f}º graus ceslsius convertiro para Fiefahrenheit é {:.0f}º Fiefahrenheits".format(c,fiefah))
--- Ex015.py
+++ Ex015.py
@@ -6,27 +6,18 @@
 #Variáveis Globais
 preco_por_dia = 60
 km_rodado = 0.15
-
-#Função calcula despesas a serem pagas pelo aluguel do carro.
-def aluguel_precp(km,dias):
-    #Fórmula calcula o preço por Km rodado pelo carro.
-    preco_km = km*km_rodado
-    print("Foram rodados {} Km, o valor a ser pago por Km rodado é de R$ {:.2f} reais ".format(km,preco_km))
-    print("\n")
-    #Fórmula calcula o preço total de dias do aluguel do carro.
-    preco_t_dias = dias*preco_por_dia
-    print("O carro foi alugado por {} dias, onde o valor a ser pago pelo total de dias é de R$ {:.2f} reias ".format(dias,preco_t_dias))
-    print("\n")
-    #Calculo de total a ser pago pelo aluguel do carro.
-    total_aluguel_carro = preco_t_dias+preco_km
-    print("O valor total a ser pago pelo alugue do carro é de R$ {:.2f} reais ".format(total_aluguel_carro))
-
     
-print("PROGRAMA CALCULA VALOR A SER PAGO PELO ALUGUEL DO CARRO\n")
+print("""PROGRAMA CALCULA VALOR A SER PAGO PELO ALUGUEL DO CARRO
+      """)
 
-km = int(input("Informe quantos km foi rodado com o carro: "))
+km = float(input("Informe quantos km foi rodado com o carro: "))
 dias = int(input("Informe a quantidade de dias que o carro foi alugado: "))
 print("\n")
 
-#Chama a função para calcular o valor a ser pago pelo alugue do carro.
-aluguel_precp(km,dias)
+#Formula: calculate the following variables.
+precoAluguel = (km*km_rodado) + (dias*preco_por_dia)
+print(f'Preço do  {km} Km rodado R$ {km*km_rodado:.2f}')
+print(f'Preço do {dias} dias locados R$ {dias*preco_por_dia}')
+
+print("O valor total a ser pago pelo alugue do carro é de R$ {:.2f} reais ".format(precoAluguel))
+
